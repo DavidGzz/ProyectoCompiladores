6d8cd328f0c46035a020d44b0cea2ef0cae14b12
David González <david_gonzalez_97@hotmail.com>
2021-10-11 10:46:19 -0500

    Cambios

--- lexer.py
+++ lexer.py
@@ -70,17 +70,93 @@ t_OR = r'\|'
 t_QUOTES = r'\"'
 t_AND = r'\&'
 
-def t_LETTER(t):
-    r'[a-zA-Z]'
+def t_INTP(t):
+    r'int'
+    t.type = reserved.get(t.value,'INT')
     return t
 
-def t_NUMBER(t):
+def t_PROGRAMA(t):
+    r'programa'
+    t.type = reserved.get(t.value,'PROGRAMA')
+    return t
+
+def t_VARS(t):
+    r'VARS | vars'
+    t.type = reserved.get(t.value,'VARS')
+    return t
+
+def t_IF(t):
+    r'if'
+    t.type = reserved.get(t.value,'IF')
+    return t
+
+def t_THEN(t):
+    r'then'
+    t.type = reserved.get(t.value,'THEN')
+    return t
+
+def t_WHILE(t):
+    r'while'
+    t.type = reserved.get(t.value,'WHILE')
+    return t
+
+def t_READ(t):
+    r'read'
+    t.type = reserved.get(t.value,'READ')
+    return t
+
+def t_WRITE(t):
+    r'write'
+    t.type = reserved.get(t.value,'WRITE')
+    return t
+
+def t_ELSE(t):
+    r'else'
+    t.type = reserved.get(t.value,'ELSE')
+    return t
+
+def t_TO(t):
+    r'to'
+    t.type = reserved.get(t.value,'TO')
+    return t
+
+def t_DO(t):
+    r'do'
+    t.type = reserved.get(t.value,'DO')
+    return t
+
+def t_FOR(t):
+    r'for'
+    t.type = reserved.get(t.value,'FOR')
+    return t
+
+def t_RETURN(t):
+    r'return'
+    t.type = reserved.get(t.value,'RETURN')
+    return t
+
+def t_CHAR(t):
+    r'char'
+    t.type = reserved.get(t.value,'CHAR')
+    return t
+
+def t_FLOATP(t):
+    r'float'
+    t.type = reserved.get(t.value,'FLOAT')
+    return t
+
+def t_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
+def t_FLOAT(t):
+    r'\d+\.\d'
+    t.value = float(t.value)
+    return t
+
 def t_ID(t):
-     r'LETTER(LETTER | NUMBER)*'
+     r'[a-zA-Z]([a-zA-Z] | \d+)*'
      t.type = reserved.get(t.value,'ID')
      return t
     
@@ -96,8 +172,44 @@ lexer = lex.lex()
 
  # Test it out
 data = '''
-if r > 5.5 then:
-    return a;
+Programa MyRike;
+VARS
+	int: i, j, p, arreglo[10];
+	float: valor;
+
+function int fact (int: j)
+VARS int i;
+	{ i = j + (p - j*2+j);
+if (j == 1) then
+	{ return (j);}
+else
+	{return(j * fact(j-1);}
+}
+
+function void calcula(int y)
+VARS int x;
+{ x = 1;
+	while(x < 11) do
+		{y = y * arreglo[x]
+		x = x+1;}
+		write( arreglo[x])
+		}
+	write("acumulado", y);
+	}
+
+	principal()
+	{ read(p); j = p*2;
+	i = fact(p);
+	for i=1 to 10 do
+		{arreglo[i] = p + i;}
+	p = Media(arreglo);
+	while(i>0) do
+		{calcula (p-1)
+		j = fact(arreglo[i]);
+		write(j, i);
+		i = i + 1;
+		}
+	}
 '''
  
  # Give the lexer some input
@@ -107,5 +219,5 @@ lexer.input(data)
 while True:
     tok = lexer.token()
     if not tok: 
-        break      # No more input
+        break     # No more input
     print(tok)
--- parser.py
+++ parser.py
@@ -149,3 +149,12 @@ def p_error(p):
     print("Syntax error in input!")
 
 parser = yacc.yacc()
+
+#while True:
+#    try:
+#        s = input('calc > ')
+#    except EOFError:
+#        break
+#    if not s: continue
+#    result = parser.parse(s)
+#    print(result)
